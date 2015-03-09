594cdf53c79401c742faaac78df8ea3ab2f0b51f
Ismael Venegas Castelló <ismael.vc1337@gmail.com>
2015-03-08 18:23:06 -0600

    Update atomic_units.jl

--- src/atomic_units.jl
+++ src/atomic_units.jl
@@ -1,38 +1,43 @@
 abstract AtomicUnit{T}
 
-const atoms_data = [
-    # Variable       Name   Symbol  Valence
-    (:hydrogen, :Hydrogen,  :H,     1),
-    (  :carbon,   :Carbon,  :C,     4)
+const atoms_meta = [
+    #     Name   Symbol  Valence
+    (:Hydrogen,  :H,     1),
+    (  :Carbon,  :C,     4)
 ]
 
-const atoms = Dict{Symbol, Tuple}[]
+const atoms_data = [
+    @compat Dict(
+        :name => name,
+        :symbol => symbol,
+        :valence => valence
+    ) for (name, symbol, valence) in atoms_meta
+]
 
-for (variable, name, symbol, valence) in atoms_data
-    @eval const $variable = Dict(:name => $name, :symbol => $symbol, :valence => $valence)
-end
-
-for (_, atom, symbol, valence) in atoms
+for (name, symbol, valence) in [values(atom) for atom in atoms_data]
     @eval begin
-        type $atom{T<:Number} <: AtomicUnit{T}
+        type $name{T<:Number} <: AtomicUnit{T}
             value::Complex{T}    # atomic value
             valence::Int         # number of valence electrons
             degree::Int          # number of shared valence electons
             free::Int            # number of free valence electrons
 
-            $atom(value) = new(value, $valence, 0, $valence)
+            $name(value) = new(value, $valence, 0, $valence)
         end
 
-        $atom{T<:Real}(value::Complex{T}) = $atom{T}(value)
-        $atom{T<:Real}(value::T) = $atom(complex(value))
-        $atom() = $atom(0.0)
+        $name{T<:Real}(value::Complex{T}) = $name{T}(value)
+        $name{T<:Real}(value::T) = $name(complex(value))
+        $name() = $name(0.0)
 
-        typealias $symbol $atom
+        typealias $symbol $name
     end
 end
 
-for atom in [atoms["name"] for atom in atoms]
-    @eval function promote_rule{T<:Real, S<:Real}(::Type{${T}}, ::Type{Carbon{S}}) = Carbon{promote_type(T, S)}
+for name in [atom[:name] for atom in atoms_data]
+    @eval function promote_rule{T<:Real, S<:Real}(::Type{$name{T}}, ::Type{$name{S}})
+        $name{promote_type(T, S)}
+    end
+end
 
 function Base.show(io::IO, a::AtomicUnit)
     print(io, "$(typeof(a))(value = $(a.value), valence = $(a.valence), degree = $(a.degree), free = $(a.free))")
